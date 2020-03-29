6a47c834de429f84d59be881e27b413b2645c199
Arthur White <mister.arthur.white@gmail.com>
2020-03-29 04:41:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const  prepend= ["Milo", "Otis", "Garfield"];
 const  removeLast= ["Milo", "Otis", "Garfield"]; 
 const  removeFirst= ["Milo", "Otis", "Garfield"]; 
 append.push("Odie");
-prepend.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
